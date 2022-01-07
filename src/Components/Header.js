ec59bd1eb1ed5526458479f8ea41919b52573761
NeenuMaria <njoshy-litmus7@madison-reed.com>
2022-01-07 11:02:14 -0600

    react-task-tracker-project

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = (props) => {
+    return (
+        <header>
+            <h1>{props.title} Task Tracker</h1>
+        </header>
+    )
+}
+
+export default Header
